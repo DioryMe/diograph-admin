579ba077057b4ec4de8b53183e16f2d14a66b93d
Jouni Alanen <jouni.alanen@leadin.fi>
2017-10-05 21:15:13 +0300

    Diory in focus form above the diory list

--- app/index.tsx
+++ app/index.tsx
@@ -5,24 +5,34 @@ import { DiographAuthentication } from "diograph-authentication"
 
 class App extends React.Component {
   state
+  inFocus
 
   constructor(props) {
     super(props)
     DiographStore.setAuthToken(DiographAuthentication.token);
-    this.state = {diories: []}
+    this.state = {diories: [], inFocus: {name: "cool", geo: {}}}
 
     DiographStore.getAllDiories().then((result) => {
       this.setState({diories: result})
+      this.setState({inFocus: this.state.diories[6]})
     })
   }
 
   render() {
     return (
-      <ul>
-       {this.state.diories.map((diory, index) => {
-         return <li key={ index }>{ diory.name }</li>;
-       })}
-      </ul>
+      <div>
+        <div>
+          <input value={this.state.inFocus.name} /><br/>
+          <input value={this.state.inFocus.geo.latitude} /><br/>
+          <input value={this.state.inFocus.geo.longitude} /><br/>
+          <button onClick={() => { console.log("Button clicked")}}>Button</button>
+        </div>
+        <ul>
+         {this.state.diories.map((diory, index) => {
+           return <li onClick={() => { this.setState({inFocus: diory})} } key={ index }>{ diory.name }</li>;
+         })}
+        </ul>
+      </div>
     )
   }
 
