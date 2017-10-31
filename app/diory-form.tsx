f1aecd8403b6ff2a0d8ec29d2cc9191d5e546ccb
Jouni Alanen <jouni.alanen@leadin.fi>
2017-10-31 22:43:32 +0200

    Date field added

--- app/diory-form.tsx
+++ app/diory-form.tsx
@@ -16,6 +16,7 @@ export class DioryForm extends React.Component<DioryFormProps, undefined> {
       <a target="_blank" href={this.props.diory.url || ""}>Address:</a> <input value={this.props.diory.url || ""} onChange={(event) => { this.props.onDioryChange({url: event.target.value}) }}  /><br/>
       Gps: <input value={this.props.diory.geo.latitude || ""} onChange={(event) => { this.props.onDioryChange({geo: {latitude: event.target.value}}) }}  />
       <input value={this.props.diory.geo.longitude || ""} onChange={(event) => { this.props.onDioryChange({geo: {longitude: event.target.value}}) }}  /><br/>
+      Date: <input value={this.props.diory.date || ""} onChange={(event) => { this.props.onDioryChange({date: event.target.value}) }}  /><br/>
       Connected diories: {this.props.diory.connectedDiories ? this.props.diory.connectedDiories.length : 0}<br/>
       <button onClick={() => { this.props.onSaveClick() }}>Save</button><br />
       <img src={this.props.diory.background || ""} width="320"/>
