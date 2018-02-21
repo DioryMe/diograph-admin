34168a3a497fe265ad1a50d4ce0b5f14d1408603
Jouni Alanen <jouni.alanen@leadin.fi>
2018-02-21 22:31:24 +0200

    Connection details reverted

--- app/connected-diories-list-item.tsx
+++ app/connected-diories-list-item.tsx
@@ -16,8 +16,14 @@ export class ConnectedDioriesListItem extends React.Component<ConnectedDioriesLi
 
       <div className="connected-diory" key={"diory" + connectedDiory.id}
         onClick={() => { this.props.onConnectedDioryClick(connectedDiory.id)} }>
-        <img width="320" src={ connectedDiory.background } />
+        <img width="320" src={ connectedDiory.background } /><br/>
         {connectedDiory.name}
+      </div>
+
+      <div className="connection" key={"connection" + connection.id}>
+        - ID: {connection.id}, fromDioryId: {connection.fromDioryId}, toDioryId: {connection.toDioryId}
+        <a onClick={() => { if(confirm("Delete the connection (id: " + connection.id + ")?")) { this.props.onDeleteConnectionClick(connection.fromDioryId, connection.toDioryId) } }}> [ X ] </a><br/>
+        -----
       </div>
 
     </div>
